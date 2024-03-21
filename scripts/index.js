eeacc5e420a18afea1589a3fa1368c71b56f9ef9
Henrietta <hetty8004@gmail.com>
2024-03-21 23:34:55 +0100

    user interface

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@ const wrapper = document.querySelector('.wrapper');
 const loginlink = document.querySelector('.login-link');
 const registerlink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
 
 registerlink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
@@ -13,5 +14,9 @@ loginlink.addEventListener('click', ()=> {
 
 
 btnPopup.addEventListener('click', ()=> {
-    wrapper.classList.add('active.popup');
+    wrapper.classList.add('active-popup');
+});
+
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.remove('active-popup');
 });
