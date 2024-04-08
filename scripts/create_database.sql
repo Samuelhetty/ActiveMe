8e3d31e149b62e031c4c1c271e232749ab769fa6
Georgen21 <chukwudinwabia42@gmail.com>
2024-04-08 22:15:23 +0100

    create tables

--- scripts/create_database.sql
+++ scripts/create_database.sql
@@ -1,7 +1,16 @@
-systemctl start mysql
+sudo service mysql start
 mysql -u root -p
-CREATE USER 'ActiveMe'@'localhost' IDENTIFIED BY '';
-CREATE DATABASE active_me_db;
-GRANT ALL PRIVILEGES ON active_me_db.* TO 'ActiveMe'@'localhost';
+CREATE USER 'ActiveMeUser'@'localhost' IDENTIFIED BY '';
+CREATE DATABASE active_me_user_db;
+GRANT ALL PRIVILEGES ON active_me_user_db.* TO 'ActiveMeUser'@'localhost';
 FLUSH PRIVILEGES;
+USE ActiveMeUser;
+CREATE TABLE ActiveMeUser.users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+SHOW TABLES;
+SHOW CREATE TABLE users;
 EXIT;
